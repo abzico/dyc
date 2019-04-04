73caf342bb89f0867144693fa17eab9cb75a9349
Wasin Thonkaew <haxpor@gmail.com>
2019-04-05 02:38:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# douyu-chat-scrapper
+
+It will help generating html file scrapping the last 20 (or configurable in the future) messages from users included a streamer itself in the chatbox ready for OBS or other streaming application to consume via browser source.
+
+# Steps To Make It Work
+
+* `cd` to `backend/` directory
+* `npm install`
+* `DYC_ROOMID=... DYC_OUTPUT_HTML_PATH=... DYC_KEEP_NUM_MSG=... node index.js`
+* Start your local web server and make sure the output generated .html file is accessible
+* Configure OBS or other streaming application's browser source to point to such .html file as well as its `css/style.css` (if need to specify it separately; which is the case for OBS), also you need to set vertical scroll offset to the farthest.
+* Now it should be working!
+
+# Environment Variables
+
+* `DYC_ROOMID` - `string` - define your room id as a streamer, you can easily check at your [Douyu account](https://mp.douyu.com/live/main). **Required**
+* `DYC_OUTPUT_HTML_PATH` - `string` - define output path for generated HTML file to be consumed and configured at OBS or other streaming application
+* `DYC_KEEP_NUM_MSG` - `string` - number of latest messages to be kept in the chatbox, messages outside of this range are not included in generated HTML file
+
+# Screenshots
+
+* Initially start
+![initially start](images/initially-start.png)
+
+* After some users commented on it
+![users commented](images/users-commented.png)
+
+# Behind The Scene
+Generated HTML file is set to refresh every 3 seconds.
+
+# License
+MIT, [ABZI.co](https://abzi.co)
---
+++ backend/index.js
@@ -0,0 +1,135 @@
+const fs = require("fs");
+
+// TODO: Prior to execute this program
+// you need to set two environment variables
+// 1. DYC_ROOMID - for streamer's room id (required)
+// 2. DYC_OUTPUT_HTML_PATH - for output path of generated html file, if empty then default to /tmp/douyu-chat.html
+// 3. DYC_KEEP_NUM_MSG - number of latest messages to keep, if not default to 20
+
+var roomID;
+if (!process.env.DYC_ROOMID)
+{
+  console.log("ROOMID environment variable needs to be set prior to start this program\n");
+  process.exit(1);
+}
+else
+{
+  roomID = process.env.DYC_ROOMID;
+}
+
+var destination_html_output = "/tmp/douyu-chat.html";
+if (process.env.DYC_OUTPUT_HTML_PATH)
+{
+  destination_html_output = process.env.DYC_OUTPUT_HTML_PATH;
+}
+
+// keep the last N messages
+// this should be enough to show in one full height of
+// chatbox in streaming app i.e. obs
+let keep_num_last_msg = 20;
+if (process.env.DYC_KEEP_NUM_MSG)
+{
+  keep_num_last_msg = process.env.DYC_KEEP_NUM_MSG;
+}
+
+// print summary of configurations used
+console.log("Output HTML at: " + destination_html_output + "\nMsgs keep: " + keep_num_last_msg + "\n");
+
+// output to html format
+function output_html(dest_path, msg_objs)
+{
+  var html_str = `<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="refresh" content="3">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+  </head>
+  <body>
+  </br>
+  </br>
+  <div class="chatbox">
+  `;
+
+  if (msg_objs.length == 0)
+  {
+    html_str += "Still quiet...</br>";
+    html_str += "You can say hi :)";
+  }
+  else
+  {
+    for (var i=0; i<msg_objs.length; ++i)
+    {
+      var msg = msg_objs[i];
+
+      if (msg.nn == 'haxpor')
+      {
+        html_str += `<div class="msg-wrapper">
+      <div class="msg-wrapper">
+          <span class="streamer">${msg_objs[i].nn}</span> <span class="msg">${msg_objs[i].txt}</span>
+      </div>`;
+
+      }
+      else
+      {
+        html_str += `<div class="msg-wrapper">
+      <div class="msg-wrapper">
+          <span class="user">${msg_objs[i].nn}</span> <span class="msg">${msg_objs[i].txt}</span>
+      </div>`;
+      }
+    }
+  }
+
+  // footer
+  html_str += "</div></body></html>";
+
+  // write into file with one call
+  fs.writeFile(dest_path, html_str, function(err) {
+    if (err)
+    {
+      console.log(err);
+      return;
+    }
+  });
+}
+
+// Import library
+var douyu = require('douyu');
+
+var room = new douyu.ChatRoom(roomID);
+
+
+
+var msg_objs = [];
+
+// System level events handler
+room.on('connect', function(message){
+	console.log('DouyuTV ChatRoom #' + roomID + ' connected.');
+
+  // write to html file even there's no msgs
+  // to indicate quiet stage
+  output_html(destination_html_output, msg_objs);
+});
+room.on('error', function(error){
+	console.error('Error: ' + error.toString());
+});
+room.on('close', function(hasError){
+	console.log('DouyuTV ChatRoom #' + roomID + ' disconnected' + hasError ? ' because of error.' : '.');
+});
+
+// Chat server events
+room.on('chatmsg', function(message){
+  console.log('[' + message.nn + ']: ' + message.txt);
+  msg_objs.push(message);
+
+  // if msg length is more than what we've set
+  // then remove the first one
+  if (msg_objs.length > keep_num_last_msg)
+  {
+    msg_objs.shift();
+  }
+
+  output_html(destination_html_output, msg_objs);
+});
+
+// Knock, knock ...
+room.open();
